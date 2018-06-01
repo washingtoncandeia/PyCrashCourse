04a7049cccddad2862cff20338adbac1513cb83c
Washington Candeia <araujowash@gmail.com>
2018-06-01 00:44:24 -0300

    v.3

---
+++ 15_Dados/rw_visual3.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+from random_walk import RandomWalk
+
+"""rw_visual.py, p.432"""
+while True:
+    # Cria um passeio aleatório e plota os pontos
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Plots
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    plt.show()
+
+    # Garantindo que podemos parar a qualquer momento
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
+ 
