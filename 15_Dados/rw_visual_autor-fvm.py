445e83ba7854a65ce31430fc128c0a2e6caa20bf
Washington Candeia <araujowash@gmail.com>
2018-06-02 15:57:54 -0300

    versao resposta do autor

---
+++ 15_Dados/rw_visual_autor-fvm.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Faça você mesmo, p.437
+# 15.3 - Movimento molecular (Código do Autor, Eric Matthes
+# )
+while True:
+    # Make a random walk, and plot the points.
+    rw = RandomWalk(5000)
+    rw.fill_walk()
+    
+    # Set the size of the plotting window.
+    plt.figure(dpi=128, figsize=(10, 6))
+    
+    point_numbers = list(range(rw.num_points))
+
+    plt.plot(rw.x_values, rw.y_values, linewidth=1)
+        
+    # Emphasize the first and last points.
+    plt.scatter(
+        0, 0, 
+        c='green', edgecolors='none', 
+        s=75
+        )
+
+    plt.scatter(
+        rw.x_values[-1], rw.y_values[-1], 
+        c='red', edgecolors='none',
+        s=75
+        )
+        
+    # Remove the axes.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+        
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
