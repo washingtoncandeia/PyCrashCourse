40b1469daf6a63dca59b1fdb1b008cfaaa087cf3
Washington Candeia <araujowash@gmail.com>
2018-10-12 00:10:20 -0300

    aliens modificado

---
+++ 6_Dicionarios/aliensmod.py
@@ -0,0 +1,23 @@
+#!/bin/env python
+# p. 159
+
+# Cria uma lista vazia para armazenar aliens.
+aliens = []
+
+for alien in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
+for alien in aliens[0:10]:
+    print(alien)
+print('...')
